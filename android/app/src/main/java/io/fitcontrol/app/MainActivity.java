8e207153578bff725daafa2225ff71b0a87e8d1a
Seu Nome Aqui <seu.email@exemplo.com>
2026-03-04 22:11:21 -0300

    feat: Add Android platform to the project.

---
+++ android/app/src/main/java/io/fitcontrol/app/MainActivity.java
@@ -0,0 +1,5 @@
+package io.fitcontrol.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.fitcontrol.app',
+  appName: 'FitControl',
+  webDir: 'dist'
+};
+
+export default config;
